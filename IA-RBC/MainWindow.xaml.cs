e0d55c261e866d5ba60d5b7ec53d2d246b96f7db
Matheus Peixoto Silvestre <6462073@univali.br>
2023-05-10 21:17:23 -0300

    commit inicial

---
+++ IA-RBC/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IA_RBC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public ObservableCollection<RbcCase> data { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            aleatoria();
+        }
+
+        ObservableCollection<RbcCase> Database = new ObservableCollection<RbcCase>()
+        {
+            new RbcCase(10, "Nathan", "Gênio"),
+            new RbcCase(10, "Matheus", "Top"),
+            new RbcCase(10, "Ries", "Foda")
+        };
+
+        private void aleatoria()
+        {
+            database.ItemsSource = this.Database;
+        }
+
+    }
+
+}
---
+++ IA-RBC/RbcCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IA_RBC
+{
+    class RbcCase
+    {
+
+        public RbcCase(int campo1, string campo2, string campo3)
+        {
+            this.Campo1 = campo1;
+            this.Campo2 = campo2;
+            this.Campo3 = campo3;
+        }
+
+        public int Campo1 { get; set; }
+        public string Campo2 { get; set; }
+        public string Campo3 { get; set; }
+
+    }
+}
